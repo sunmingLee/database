93aa67207e5dc39ee93f25f652f503e873e5c1d5
sunmingLee <25thbam@gmail.com>
2021-05-14 16:54:07 +0900

    Update error page

--- web_application/mysite/mysite/views.py
+++ web_application/mysite/mysite/views.py
@@ -43,13 +43,13 @@ def result(request):
     return render(request, 'result.html', {'result':result,'visitors':visitors})
 
 def bad_request(request, exception):
-    return render(request, 'error_404.html', status = 400)
+    return render(request, 'error_400.html', status = 400)
 
-def page_not_found(request):
+def page_not_found(request, exception):
     return render(request, 'error_404.html', status = 404)
 
 def server_error(request):
-    return render(request, 'error_404.html', status = 500)
+    return render(request, 'error_500.html', status = 500)
     
 def index(request):
    return render(request, 'index.html')
