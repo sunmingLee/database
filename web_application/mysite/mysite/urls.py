fcf934ab4ab98b704de9ee28f20b87c45681465a
sunmingLee <25thbam@gmail.com>
2021-05-18 09:29:26 +0900

    Adjust result font size

--- web_application/mysite/mysite/urls.py
+++ web_application/mysite/mysite/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path('map/', views.map, name='map'),
     path('contact/',views.contact, name='contact'),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT})
-    #re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT})
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
 
